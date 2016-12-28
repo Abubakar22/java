9a52e32007a9c60a85cc7d1663f6d8d8d1808078
root <root@ip-172-31-36-35.us-west-2.compute.internal>
2016-12-28 04:02:57 +0000

    change again

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-	System.out.println("Hello World Munchkins-GITHUB ");
+	System.out.println("Bolo Hello World Munchkins-GITHUB ");
 }
 }
