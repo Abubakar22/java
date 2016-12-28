9226f203cd13267f7db942fa8f8536b8ff5f617f
root <root@ip-172-31-36-35.us-west-2.compute.internal>
2016-12-28 03:58:31 +0000

    change java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-	System.out.println("Hello World Jenkins-GITHUB ");
+	System.out.println("Hello World Munchkins-GITHUB ");
 }
 }
